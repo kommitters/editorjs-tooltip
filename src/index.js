fa90c3c4f596f2e502daa2f0710124336856c21d
Juan David Gaviria Agudelo <45491405+MrJuanGaviriaK@users.noreply.github.com>
2021-10-13 14:15:14 -0500

    #9-Add the tooltip location (#21)
    * Add tooltip location
    
    * Refactor location logic

--- src/index.js
+++ src/index.js
@@ -34,16 +34,19 @@ export default class Tooltip {
   /**
    * @param {object} api Editor.js api
    */
-  constructor({ api }) {
+  constructor({ api, config = {} }) {
     this.api = api;
     this.button = null;
     this._state = false;
     this.spanTooltip = null;
 
+    const { location = 'bottom' } = config;
+    this.tooltipLocation = location;
+
     this.tag = 'SPAN';
 
     this.CSS = {
-      input: 'tooltip-tool__input'
+      input: 'tooltip-tool__input',
     };
   }
   /**
@@ -145,7 +148,8 @@ export default class Tooltip {
       if (e.key === 'Enter') {
         const tooltipValue = this.tooltipInput.value;
         this.spanTooltip.dataset.tooltip = tooltipValue;
-        this.api.tooltip.onHover(this.spanTooltip, tooltipValue);
+        const { tooltipLocation } = this;
+        this.api.tooltip.onHover(this.spanTooltip, tooltipValue, { placement: tooltipLocation });
         this.closeToolbar();
       }
     }, false);
