1ef72df65ff19074181fe95a140cd2bcd0a86b2f
Juan Gaviria <juan.gaviria@kommit.co>
2021-10-20 22:09:37 -0500

    Add basic tests to tooltip component

--- test/tooltip.test.js
+++ test/tooltip.test.js
@@ -1,3 +1,46 @@
-test('initial Tooltip test', () => {
-  expect(true).toBe(true);
+/**
+ * @jest-environment jsdom
+ */
+import { createTooltip, createTooltipRightPosition } from './fixtures/tooltip';
+
+describe('Tooltip', () => {
+  let tooltip;
+
+  describe('validates panel components', () => {
+    beforeEach(() => {
+      document.body.innerHTML = '<h3 id="tooltip">Tooltip text</h3>';
+      tooltip = createTooltip();
+    });
+
+    it('validates tooltip button', () => {
+      const button = tooltip.render();
+      expect(button).toHaveClass('ce-inline-tool');
+    });
+
+    it('validates tooltip input', () => {
+      const input = tooltip.renderActions();
+      expect(input).toHaveClass('tooltip-tool__input');
+      expect(input.placeholder).toBe('Add a tooltip');
+    });
+
+    it('validates tooltip creation', () => {
+      const title = document.getElementById('tooltip');
+      const range = document.createRange();
+      range.selectNode(title);
+      tooltip.surround(range);
+
+      const tooltipElement = document.getElementsByClassName('cdx-tooltip')[0];
+
+      expect(tooltipElement).toBeInTheDocument();
+      expect(tooltipElement).toHaveTextContent('Tooltip text');
+      expect(tooltip.tooltipLocation).toBe('bottom');
+    });
+  });
+
+  describe('validates tooltip position', () => {
+    it('validates position', () => {
+      tooltip = createTooltipRightPosition();
+      expect(tooltip.tooltipLocation).toBe('right');
+    });
+  });
 });
